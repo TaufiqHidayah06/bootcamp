174c1bc268f678319c60b4c034f2eb719d060911
TaufiqHidayah6 <taufiqhidayah0611@gmail.com>
2023-07-02 03:17:10 +0700

    Quiz Bonus, Bagian FormTerpisah, inputnya terpisah

---
+++ Quiz3/Bonus/FormTerpisah.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Form Add User</title>
+</head>
+
+<body>
+    <h1>Form Add User</h1>
+    <form method="POST" action="">
+        <label for="inama">Masukkan Nama:</label><br>
+        <input type="text" id="inama" name="inama"><br><br>
+        <label for="iumur">Masukkan Umur:</label><br>
+        <input type="text" id="iumur" name="iumur"><br><br>
+        <label for="ikota">Masukkan Kota:</label><br>
+        <input type="text" id="ikota" name="ikota"><br><br>
+        <input type="submit" value="Submit">
+    </form><br><br>
+</body>
+<?php
+
+class Form
+{
+    private static $idCounter = 1;
+    private $id;
+    private $nama;
+    private $umur;
+    private $kota;
+    
+
+    public function __construct($nama, $umur, $kota)
+    {
+        $this->id = self::$idCounter++;
+        $this->nama = strtoupper($nama);
+        $this->umur = $this->penulisan($umur);
+        $this->kota = strtoupper($kota);
+    }
+    private function penulisan($umur)
+    {
+        $umur = preg_replace('/\b(TAHUN|THN|TH)\b/i', '', $umur);
+        return trim($umur);
+    }
+
+    public function validasi()
+    {
+        if (empty($this->nama) || empty($this->umur) || empty($this->kota)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public function save()
+    {
+        if ($this->validasi()) {
+            echo "Nomor 1 : <br>";
+            echo $this->nama." ".$this->umur." ".$this->kota."<br><br>";
+            echo "Nomor 2 : <br>";
+            echo "Nama : ".$this->nama.", Umur : ".$this->umur.", Kota : ".$this->kota."<br><br>";
+            echo "Nomor 4 : <br>";
+            echo 
+            "<table border='1' style='text-align: center;'>
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Nama</th>
+                        <th>Umur</th>
+                        <th>Kota</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>" .$this->id. "</td>
+                        <td>" .$this->nama. "</td>
+                        <td>" .$this->umur. "</td>
+                        <td>" .$this->kota. "</td>
+                    </tr>
+                </tbody>
+            </table>";
+        } else {
+            echo "Data tidak valid. Mohon isi semua field.";
+        }
+    }
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+$nama = $_POST['inama']?? '';
+$umur = $_POST['iumur']?? '';
+$kota = $_POST['ikota']?? '';
+
+$userForm = new Form($nama, $umur, $kota);
+$userForm->save();
+}
+
+?>
+
+</html>
